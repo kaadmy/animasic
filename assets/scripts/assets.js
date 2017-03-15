a14e100bed8152df8fbb375ebbafadf8d213f84d
KaadmY <kaadmy@gmail.com>
2017-03-14 19:06:45 -0700

    A few fixes

--- assets/scripts/assets.js
+++ assets/scripts/assets.js
@@ -16,7 +16,7 @@ AssetLoader.prototype.addQueueItem=function(name, done) {
 
         return
     }
-    
+
     this.queue.push({
         name: name,
         done: done
@@ -30,19 +30,19 @@ AssetLoader.prototype.addQueueItem=function(name, done) {
 AssetLoader.prototype.loadNextQueueItem=function(done) {
     if (!done) {
         console.error("No finish function given; this is usually bad");
-        
+
         return;
     }
 
     // this should only happen if you called this function with an empty queue
-    if (this.queue.length<1) { 
+    if (this.queue.length<1) {
         console.log("No queue items left");
 
         done();
 
         return;
     }
-    
+
     var item=this.queue[0];
 
     var dataType="text";
@@ -51,18 +51,24 @@ AssetLoader.prototype.loadNextQueueItem=function(done) {
         dataType="json";
     }
 
+    console.log("Loading asset "+item.name+", type "+dataType+"...");
+
     $.get(item.name, "", function(data) {
         this.finishedItems++;
 
-        cache[item.name]={
+        this.cache[item.name]={
             data: data
         }
 
         if (item.done) {
-            cache[item.name].value=item.done(item.name, data);
+            this.cache[item.name].value=item.done(item.name, data);
         } else {
-            cache[item.name].value=true;
+            this.cache[item.name].value=true;
         }
+
+        this.queue.splice(0, 1);
+
+        this.loadNextQueueItem(done);
 
         if (queue.length<1) {
             done();
@@ -70,8 +76,5 @@ AssetLoader.prototype.loadNextQueueItem=function(done) {
             return;
         }
 
-        this.queue.splice(0, 1);
-
-        this.loadNextQueueItem(done);
     }, dataType);
 }
